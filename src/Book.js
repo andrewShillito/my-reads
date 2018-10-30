415047b4863c4db0cee7f52d62f412228baaf2a0
Andrew Shillito <andrew.shillito88@gmail.com>
2018-10-30 10:53:34 +0000

    refactor: change Book component to stateless func

--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,28 @@
 import React from "react";
 
-class Book extends React.Component {
-    state = {
-        shelf: this.props.shelf,
-    }
-    render() {
-        return (
-            <div className="book">
-                <div className="book-img-container">
-                    <img 
-                        className="book-img"
-                        src={this.props.img}
-                        alt={`${this.props.title} book cover`}
-                    />
-                    <select className="book-select-menu" onChange={(event) => this.props.onBookMove(this.props.book, event.target.value)}>
-                        <optgroup label="Move to...">
-                            <option value="" selected disabled hidden></option>
-                            <option value="currentlyReading" disabled={this.props.shelf==="currentlyReading"}>Currently Reading</option>
-                            <option value="wantToRead" disabled={this.props.shelf==="wantToRead"}>Want to Read</option>
-                            <option value="read" disabled={this.props.shelf==="read"}>Read</option>
-                            <option value="none" disabled={this.props.shelf==="none"}>None</option> 
-                        </optgroup>
-                    </select>
-                </div>
-                <h4 className="book-title">{this.props.title}</h4>
-                <span className="book-author">{this.props.author}</span>
+function Book(props){
+    return (
+        <div className="book">
+            <div className="book-img-container">
+                <img 
+                    className="book-img"
+                    src={props.img}
+                    alt={`${props.title} book cover`}
+                />
+                <select className="book-select-menu" defaultValue={props.shelf} onChange={(event) => props.onBookMove(props.book, event.target.value)}>
+                    <optgroup label="Move to...">
+                        <option value="" selected disabled hidden></option>
+                        <option value="currentlyReading" disabled={props.shelf==="currentlyReading"}>Currently Reading</option>
+                        <option value="wantToRead" disabled={props.shelf==="wantToRead"}>Want to Read</option>
+                        <option value="read" disabled={props.shelf==="read"}>Read</option>
+                        <option value="none" disabled={props.shelf==="none"}>None</option> 
+                    </optgroup>
+                </select>
             </div>
-        )
-    }
+            <h4 className="book-title">{props.title}</h4>
+            <span className="book-author">{props.author}</span>
+        </div>
+    );
 }
 
 export default Book;
